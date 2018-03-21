ecab85a3e9dcbc1ab3953b56ed3a903ea1c30027
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-03-21 13:04:18 +0100

    [Token] Added more information fields.

--- src/tokens/Token.java
+++ src/tokens/Token.java
@@ -1,16 +1,21 @@
 package tokens;
 
 public class Token {
-    private String tokenValue;
-    private TokenType tokenType;
+    private final String tokenValue;
+    private final TokenType tokenType;
+    private final Object literal;
+    private final int line;
 
-    public Token(String tokenValue, TokenType tokenType){
+    public Token(String tokenValue, TokenType tokenType, Object literal, int line) {
         this.tokenValue = tokenValue;
         this.tokenType = tokenType;
+        this.literal = literal;
+        this.line = line;
     }
 
     @Override
-    public String toString(){
-        return "Type: "+tokenType+", token value: "+tokenValue+"\n";
+    public String toString() {
+        return "Type: " + tokenType + ", value: " + tokenValue +
+                " literal: " + literal + ", line: " + line + "\n";
     }
 }
