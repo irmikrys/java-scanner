7e63f871119a6ed91d13c580d83fc165ceddcfc4
Irmina Krysiak <irmina.krysiak@gmail.com>
2018-03-21 11:58:36 +0100

    [Resources] Test file added to project.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,26 @@ import scanner.Scanner;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
+        Main main = new Main();
+        File file = main.getFile();
         Charset encoding = Charset.defaultCharset();
-        File file = new File("../Input.txt");
         Scanner scanner = new Scanner(file);
         try {
             scanner.scanFile(encoding);
         } catch (IOException e) {
             System.out.println("An IOException caught while handling a file...");
         }
+    }
+
+    private File getFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(Objects.requireNonNull(
+                classLoader.getResource("file/Input.txt")).getFile()
+        );
     }
 }
